5f39667ef0c9131ae6788002f9ca96b0fe599bb7
Rutik3023 <rutikthorat3023@gmail.com>
2024-11-13 21:55:05 +0530

    doc

---
+++ AdminPanel/Controllers/PatientController.cs
@@ -0,0 +1,25 @@
+﻿using AdminPanel.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminPanel.Controllers
+{
+    public class PatientController : Controller
+    {
+        public IActionResult form()
+        {
+            return View();
+        }
+
+
+        public IActionResult form( Patientmaster data)
+        {
+            return View();
+        }
+
+
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
---
+++ AdminPanel/Models/Patientmaster.cs
@@ -0,0 +1,23 @@
+﻿namespace AdminPanel.Models
+{
+    public class Patientmaster
+    {
+        public int syscode { get; set; }
+
+        public string orgcode { get; set; }
+        public String Name { get; set; }
+        public string Email { get; set; }
+        public string MobileNo { get; set; }
+        public string Address { get; set; }
+        public string AdhaarNo { get; set; }
+        public int BloodGroup { get; set; }
+        public char Gender { get; set; }
+       
+        public DateTime CreatedOn { get; set; }
+        public int CreatedBy { get; set; }
+
+
+
+
+    }
+}
